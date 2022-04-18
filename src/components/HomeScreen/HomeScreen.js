bed38e9c01a639f7a6fc2cf8b1285d3cec728068
Nienke Dekker <nienkedekker@gmail.com>
2022-04-18 13:00:29 +0200

    small css and copy fixes

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -8,8 +8,8 @@ export default function HomeScreen({ createCall, startJoiningCall }) {
 
   return (
     <div className="home-screen">
-      <h1>Call Object React demo</h1>
-      <p>Start demo with a new unique room by clicking the button below.</p>
+      <h1>React Daily Hooks custom video app</h1>
+      <p>Start the demo with a new unique room by clicking the button below.</p>
       <button onClick={startCall}>Click to start a call</button>
       <p className="small">
         Select “Allow” to use your camera and mic for this call if prompted
