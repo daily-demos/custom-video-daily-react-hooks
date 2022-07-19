99f59475476302fad737d96f02268f726575b18e
Jessica Mitchell <jess@pluot.co>
2022-07-19 17:05:56 -0400

    cometchat integration wip

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,91 +1,88 @@
-import { useCallback, useState } from 'react';
-import { useAppMessage, useLocalParticipant } from '@daily-co/daily-react-hooks';
+import { useCallback, useState, useEffect } from 'react';
+import { useLocalParticipant, useRoom } from '@daily-co/daily-react-hooks';
 
 import { Arrow } from '../Tray/Icons/index';
 import './Chat.css';
 
-export default function Chat({ showChat, toggleChat }) {
+export default function Chat({ showChat }) {
   const localParticipant = useLocalParticipant();
-  const [messages, setMessages] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const { name: roomName } = useRoom();
 
-  const sendAppMessage = useAppMessage({
-    onAppMessage: useCallback(
-      (ev) =>
-        setMessages((messages) => [
-          ...messages,
-          {
-            msg: ev.data.msg,
-            name: ev.data.name,
-          },
-        ]),
-      [],
-    ),
-  });
+  const [loaded, setLoaded] = useState(false);
 
-  const sendMessage = useCallback(
-    (message) => {
-      /* Send the message to all participants in the chat - this does not include ourselves!
-       * See https://docs.daily.co/reference/daily-js/events/participant-events#app-message
-       */
-      sendAppMessage(
-        {
-          msg: message,
-          name: localParticipant?.user_name || 'Guest',
-        },
-        '*',
-      );
-
-      /* Since we don't receive our own messages, we will set our message in the messages array.
-       * This way _we_ can also see what we wrote.
-       */
-      setMessages([
-        ...messages,
-        {
-          msg: message,
-          name: localParticipant?.user_name || 'Guest',
-        },
-      ]);
-    },
-    [localParticipant, messages, sendAppMessage],
+  const launchCometChat = useCallback(
+    () =>
+      window.CometChatWidget.launch({
+        widgetID: process.env.REACT_APP_COMET_CHAT_WIDGET_ID,
+        target: '#cometchat',
+        roundedCorners: 'false',
+        height: '100%',
+        width: '100%',
+        defaultID: roomName, //default UID (user) or GUID (group) to show,
+        defaultType: 'group', //user or group
+      }),
+    [roomName],
   );
+  const createCometChatGroup = () => {
+    const GUID = roomName;
+    const GROUP_NAME = roomName;
+    const GROUP_TYPE = 'public';
+    const group = new window.CometChatWidget.CometChat.Group(GUID, GROUP_NAME, GROUP_TYPE);
 
-  const onChange = (e) => {
-    setInputValue(e.target.value);
+    // CREATE GROUP WITH ROOM INFO
+    window.CometChatWidget.createOrUpdateGroup(group)
+      .then((group) => {
+        // Proceed with launching your Chat Widget
+        launchCometChat();
+      })
+      .catch((e) => {
+        // If the group already exists, launch chat without trying to create
+        // the group first
+        if (e.code === 'ERR_GROUP_NOT_JOINED') {
+          launchCometChat();
+        }
+      });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!inputValue) return; // don't allow people to submit empty strings
-    sendMessage(inputValue);
-    setInputValue('');
-  };
+  // INITIATE COMETCHAT WIDGET!
+  useEffect(() => {
+    if (loaded) return;
+    // INITIALIZE COMETCHAT WIDGET
+    window.CometChatWidget.init({
+      appID: process.env.REACT_APP_COMET_CHAT_APP_ID,
+      appRegion: process.env.REACT_APP_COMET_CHAT_APP_REGION,
+      authKey: process.env.REACT_APP_COMET_CHAT_APP_AUTH_KEY,
+    }).then(
+      () => {
+        const UID = localParticipant.session_id;
+        const user = new window.CometChatWidget.CometChat.User(UID);
+        user.setName(localParticipant.user_name);
 
-  return showChat ? (
-    <aside className="chat">
-      <button onClick={toggleChat} className="close-chat">Close chat</button>
-      <ul className="chat-messages">
-        {messages?.map((message, index) => (
-          <li key={`message-${index}`} className="chat-message">
-            <span className="chat-message-author">{message?.name}</span>:{' '}
-            <p className="chat-message-body">{message?.msg}</p>
-          </li>
-        ))}
-      </ul>
-      <div className="add-message">
-        <form className="chat-form" onSubmit={handleSubmit}>
-          <input
-            className="chat-input"
-            type="text"
-            placeholder="Type your message here.."
-            value={inputValue}
-            onChange={(e) => onChange(e)}
-          />
-          <button type="submit" className="chat-submit-button">
-            <Arrow />
-          </button>
-        </form>
-      </div>
+        // CREATE USER WITH LOCAL PARTICIPANT INFO
+        window.CometChatWidget.createOrUpdateUser(user).then((user) => {
+          // Proceed with user login
+          window.CometChatWidget.login({
+            uid: UID,
+          }).then(() => {
+            createCometChatGroup();
+
+            setLoaded(true);
+          });
+        });
+      },
+      (error) => {
+        console.log('Initialization failed with error:', error);
+        //Check the reason for error and take appropriate action.
+      },
+    );
+  }, [localParticipant.session_id, localParticipant.user_name, loaded, roomName]);
+
+  return (
+    <aside className={`chat ${!showChat ? 'hidden' : ''}`}>
+      {/* <button onClick={toggleChat} className="close-chat">
+        <Arrow />
+      </button> */}
+      <div id="cometchat"></div>
     </aside>
-  ) : null;
+  );
 }
