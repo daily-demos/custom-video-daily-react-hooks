638f81c7e12d08a8ffe64139d6532f93c87f4499
Nienke Dekker <nienkedekker@gmail.com>
2023-01-10 20:18:39 +0100

    Bump versions of daily-js and daily react, update link in readme, update text in Homescreen

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -10,7 +10,7 @@ export default function HomeScreen({ createCall, startHairCheck }) {
 
   return (
     <div className="home-screen">
-      <h1>React Daily Hooks custom video app</h1>
+      <h1>Daily React custom video application</h1>
       <p>Start the demo with a new unique room by clicking the button below.</p>
       <button onClick={startDemo} type="button">Click to start a call</button>
       <p className="small">Select “Allow” to use your camera and mic for this call if prompted</p>
