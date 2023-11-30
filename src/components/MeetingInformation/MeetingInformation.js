c869f5c4a7370c48749e7f54bcc90d5807eb44cd
Christian Stuff <christian@daily.co>
2023-11-30 09:27:31 +0100

    update packages and code

--- src/components/MeetingInformation/MeetingInformation.js
+++ src/components/MeetingInformation/MeetingInformation.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { useDaily, useNetwork, useParticipantIds, useRoom } from '@daily-co/daily-react';
+import { useMeetingState, useNetwork, useParticipantIds, useRoom } from '@daily-co/daily-react';
 import './MeetingInformation.css';
 
 export default function MeetingInformation() {
-  const callObject = useDaily();
   const room = useRoom();
   const network = useNetwork();
-  const allParticipants = useParticipantIds()?.toString();
+  const allParticipants = useParticipantIds();
+  const meetingState = useMeetingState();
 
   return (
     <section className="meeting-information">
       <h1>Meeting information</h1>
       <ul>
-        <li>Meeting state: {callObject?.meetingState() ?? 'unknown'}</li>
-        <li>Room ID: {room?.id ?? 'unknown'}</li>
+        <li>Meeting state: {meetingState ?? 'unknown'}</li>
+        <li>Meeting ID: {room?.id ?? 'unknown'}</li>
         <li>Room name: {room?.name ?? 'unknown'}</li>
         <li>Network status: {network?.threshold ?? 'unknown'}</li>
         <li>Network topology: {network?.topology ?? 'unknown'}</li>
-        <li>Participant IDs: {allParticipants}</li>
+        <li>Participant IDs: {allParticipants.join(', ')}</li>
       </ul>
     </section>
   );
